9211ca4459976a6086c3d56060a1a12c46aced02
NATSU03 <vishwasw75@gmail.com>
2022-11-05 22:30:37 +0530

    JWT-2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://vishwasw75:9595335675@firstcluster.jde07cq.mongodb.net/Auth", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,46 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
+const jwt = require('jsonwebtoken');
+const userModel = require("../models/userModel")
 
+const Authenticate = async function(req, res, next) {
+    
+  let userId = req.params.userId
+
+  let user = await userModel.findById(userId);
+
+  if (user.isdeleted==true)
+    return res.send ({status :true,msg: "user is deleted "})
+
+  if (!user)
+    return res.send({ status: false, msg: "No such user exists" });
+
+  
+  
     next()
 }
 
 
-const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
+const Authorise = function(req, res, next) {
+    
+    let user = req.params.userId
+
+    let token = req.headers["x-auth-token"];
+  
+    if (!token) return res.send({ status: false, msg: "token is missing" });
+
+    let decodedToken = jwt.verify(token, "Thousand-Years-Blood-war");
+
+    if (!decodedToken)
+
+    return res.send({ status: false, msg: "token is invalid" });
+  
+    let userLoggedIn = decodedToken.userId
+  
+    if(user != userLoggedIn) return res.send
+    ({status: false, msg: 'you are not allowed to perform this action '})
+
+
     next()
 }
+
+module.exports.Authenticate = Authenticate
+module.exports.Authorise = Authorise

6d61020d06372ea7236e7084b5e7a5145d28d431
NATSU03 <vishwasw75@gmail.com>
2022-11-08 18:37:58 +0530

    axios/assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -10,7 +10,9 @@ router.get("/test-me", function (req, res) {
 
 
 router.get("/cowin/states", CowinController.getStates)
+
 router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
+
 router.get("/cowin/getByPin", CowinController.getByPin)
 
 router.post("/cowin/getOtp", CowinController.getOtp)

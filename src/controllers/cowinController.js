eaa2358519e9868c4d04ff0ace6fd750651a1144
NATSU03 <vishwasw75@gmail.com>
2022-11-09 20:47:10 +0530

    memecreator

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -1,18 +1,30 @@
 let axios = require("axios")
 
 
-let getStates = async function (req, res) {
+
+
+let CreateMeme = async function (req, res) {
 
     try {
-        let options = {
-            method: 'get',
-            url: 'https://cdn-api.co-vin.in/api/v2/admin/location/states'
+
+        let MemeFormatID = req.query.template_id
+        let Text0 =req.query.text0
+        let Text1 = req.query.text1 
+        let UserName = req.query.username
+        let pass = req.query.password
+
+        const option =  {
+            method :'post',
+            url: `https://api.imgflip.com/caption_image? template_id=${MemeFormatID}&text0=${Text0}&text1=${Text1}&username=${UserName}&password=${pass}`
         }
-        let result = await axios(options);
-        console.log(result)
-        let data = result.data
-        res.status(200).send({ msg: data, status: true })
+
+        let MemeCreated = await axios(option)
+
+        res.status(200).send({MemeByMemeLORD:MemeCreated.data})
+
+
     }
+
     catch (err) {
         console.log(err)
         res.status(500).send({ msg: err.message })
@@ -20,66 +32,14 @@ let getStates = async function (req, res) {
 }
 
 
-let getDistricts = async function (req, res) {
-    try {
-        let id = req.params.stateId
-        let options = {
-            method: "get",
-            url: `https://cdn-api.co-vin.in/api/v2/admin/location/districts/${id}`
-        }
-        let result = await axios(options);
-        console.log(result)
-        let data = result.data
-        res.status(200).send({ msg: data, status: true })
-    }
-    catch (err) {
-        console.log(err)
-        res.status(500).send({ msg: err.message })
-    }
-}
-
-let getByPin = async function (req, res) {
-    try {
-        let pin = req.query.pincode
-        let date = req.query.date
-        console.log(`query params are: ${pin} ${date}`)
-        var options = {
-            method: "get",
-            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pin}&date=${date}`
-        }
-        let result = await axios(options)
-        console.log(result.data)
-        res.status(200).send({ msg: result.data })
-    }
-    catch (err) {
-        console.log(err)
-        res.status(500).send({ msg: err.message })
-    }
-}
-
-let getOtp = async function (req, res) {
-    try {
-        let blahhh = req.body
-        
-        console.log(`body is : ${blahhh} `)
-        var options = {
-            method: "post",
-            url: `https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP`,
-            data: blahhh
-        }
-
-        let result = await axios(options)
-        console.log(result.data)
-        res.status(200).send({ msg: result.data })
-    }
-    catch (err) {
-        console.log(err)
-        res.status(500).send({ msg: err.message })
-    }
-}
+module.exports.CreateMeme = CreateMeme
 
 
-module.exports.getStates = getStates
-module.exports.getDistricts = getDistricts
-module.exports.getByPin = getByPin
-module.exports.getOtp = getOtp
+// var options = {
+//     method: "post",
+//     url: `https://api.imgflip.com/caption_image? template_id=${MemeFormatID}&text0=${Text0}&text1=${Text1}&username=${UserName}&password=${pass}`
+// }
+// let result = await axios(options)
+// console.log(result.data)
+// res.status(200).send({ msg: result.data })
+
